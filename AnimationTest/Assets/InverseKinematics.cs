8053ace516b1cb49915b21fd15dc20ae46250ac4
Student John O Sullivan (TL_KCPGD_B_Y4) <t00234079@mymtu.ie>
2025-12-03 10:21:51 +0000

    Signed-off-by: Student John O Sullivan (TL_KCPGD_B_Y4) <t00234079@mymtu.ie>

--- AnimationTest/Assets/InverseKinematics.cs
+++ AnimationTest/Assets/InverseKinematics.cs
@@ -29,14 +29,14 @@ public class InverseKinematics : MonoBehaviour
                     // Set the weights to their maximum value of 1
                     animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
                     animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
-                    // Set the IK Goals for the character so it knows where to look for the object
+                    // Set the IK Goals for the character so it knows where to touch the cylinder
                     animator.SetIKPosition(AvatarIKGoal.RightHand, rightHand.position);
                     animator.SetIKRotation(AvatarIKGoal.RightHand, rightHand.rotation);
                 }
             }
             else
             {
-                // Set all weights to 0 so the character's animation does not change
+                // Set all weights to 0 so the character's hand position does not change
                 animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
                 animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
                 animator.SetLookAtWeight(0);
